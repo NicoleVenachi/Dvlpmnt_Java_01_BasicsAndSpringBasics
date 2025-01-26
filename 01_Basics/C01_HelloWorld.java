d165158655e74193804920c1c32f599f24a2ecc3
nicolevenachi <ludwing@unicauca.edu.co>
2025-01-26 16:50:35 -0500

    01_Basics->03_packagesAndEclipse

--- 01_Basics/C01_HelloWorld.java
+++ 01_Basics/C01_HelloWorld.java
@@ -1,6 +1,9 @@
-public class C01_HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+package com.test;
 
+public class C01HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
