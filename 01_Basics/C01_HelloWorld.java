ab8fd4b2169e01b2b66d550de6a5d5a2455589ff
nicolevenachi <ludwing@unicauca.edu.co>
2025-02-01 23:22:36 -0500

    01_Basics->04_JavaComments

--- 01_Basics/C01_HelloWorld.java
+++ 01_Basics/C01_HelloWorld.java
@@ -1,9 +1,22 @@
 package com.test;
 
+/**
+ * Hello word aplication
+ * @author Nicole
+ */
 public class C01HelloWorld {
-
+	/**
+	 * Program entry point
+	 * @param args input arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		/*
+		 * Please, do not remove
+		 * this code 
+		 */
+		
+		// TODO add more code here in the future
 	}
 
 }
